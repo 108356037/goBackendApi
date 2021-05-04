17e97dd5822d255ae57f196f2e5fb81ac94dbff9
Tsai Wei-Che <108356037@nccu.edu.tw>
2021-05-04 17:41:08 +0800

    complete create user and handler

--- ticketing/auth/models/users.go
+++ ticketing/auth/models/users.go
@@ -1,13 +1,41 @@
 package models
 
-// database "github.com/108356037/goticketapp/auth/internal/pkg/db/postgres"
+import (
+	database "github.com/108356037/goticketapp/auth/internal/pkg/db/postgres"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	// probably need id from db
+	UserId   int    `json:"userId"`
 }
 
-func CreateUser(users *User) error {
-	return nil
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func CreateUser(users *User) (int, error) {
+	stmt, err := database.Db.Prepare("INSERT INTO users(email,password) VALUES($1,$2) RETURNING user_id")
+	if err != nil {
+		return -1, err
+	}
+
+	hashedpasswd, err := HashPassword(users.Password)
+	if err != nil {
+		return -1, err
+	}
+
+	var userId int
+	err = stmt.QueryRow(users.Email, hashedpasswd).Scan(&userId)
+	if err != nil {
+		return -1, err
+	}
+	return userId, nil
 }
--- ticketing/auth/routes/signup.go
+++ ticketing/auth/routes/signup.go
@@ -2,13 +2,9 @@ package routes
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"strings"
 
+	"github.com/108356037/goticketapp/auth/middleware"
 	"github.com/108356037/goticketapp/auth/models"
 	"github.com/golang/gddo/httputil/header"
 )
@@ -21,7 +17,7 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 		value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
 		if value != "application/json" {
 			msg := "Content-Type header is not application/json"
-			http.Error(w, msg, http.StatusUnsupportedMediaType)
+			middleware.JSONError(w, msg, http.StatusUnsupportedMediaType)
 			return
 		}
 	}
@@ -35,70 +31,25 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	user := models.User{}
 	err := dec.Decode(&user)
 
-	// below logic is for any error that happens in request decode stage
-	// * can be in common middleware *
+	// below logic is for any error that happens in request-body decode stage
 	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-
-		switch {
-		// Catch any syntax errors in the JSON and send an error message
-		// which interpolates the location of the problem to make it
-		// easier for the client to fix.
-		case errors.As(err, &syntaxError):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// In some circumstances Decode() may also return an
-		// io.ErrUnexpectedEOF error for syntax errors in the JSON. There
-		// is an open issue regarding this at
-		// https://github.com/golang/go/issues/25956.
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON")
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// Catch any type errors, like trying to assign a string in the
-		// JSON request body to a int field in our Person struct. We can
-		// interpolate the relevant field name and position into the error
-		// message to make it easier for the client to fix.
-		case errors.As(err, &unmarshalTypeError):
-			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// Catch the error caused by extra unexpected fields in the request
-		// body. We extract the field name from the error message and
-		// interpolate it in our custom error message. There is an open
-		// issue at https://github.com/golang/go/issues/29035 regarding
-		// turning this into a sentinel error.
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// An io.EOF error is returned by Decode() if the request body is
-		// empty.
-		case errors.Is(err, io.EOF):
-			msg := "Request body must not be empty"
-			http.Error(w, msg, http.StatusBadRequest)
-
-		// Catch the error caused by the request body being too large. Again
-		// there is an open issue regarding turning this into a sentinel
-		// error at https://github.com/golang/go/issues/30715.
-		case err.Error() == "http: request body too large":
-			msg := "Request body must not be larger than 1MB"
-			http.Error(w, msg, http.StatusRequestEntityTooLarge)
-
-		// Otherwise default to logging the error and sending a 500 Internal
-		// Server Error response.
-		default:
-			log.Println(err.Error())
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		}
+		middleware.RequestBodyError(w, err)
 		return
 	}
 
-	fmt.Printf("%v\n", user.Email)
-	fmt.Printf("%v\n", user.Password)
-	fmt.Fprintf(w, "User: %+v", user)
+	w.Header().Set("Content-Type", "application/json")
+	userId, err := models.CreateUser(&user)
+	user.UserId = userId
+	if err != nil {
+		middleware.JSONError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	userJs, err := json.Marshal(user)
+	if err != nil {
+		middleware.JSONError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(userJs)
 
 }
